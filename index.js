f8c34b47399241ea9fab92345c0149a40e117336
kartikrana77 <kartikrana9938@gmail.com>
2024-06-12 18:18:49 +0530

    new calculator style

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 let displayCount = '';
-document.querySelector('#display').value = displayCount;
+let input = document.querySelector('#display');
+let button = document.querySelectorAll('.button');
+
+Array.from(button).forEach((button)=>{
+  button. addEventListener('click',(e)=>{
+        if(e.target.innerHTML === '='){
+            displayCount = eval(displayCount);
+            input.value = displayCount;
+        }
+        else if(e.target.innerHTML === 'AC'){
+            displayCount = '';
+            input.value = displayCount;
+        }
+        else{
+            displayCount += e.target.innerHTML;
+            input.value = displayCount;
+        }
+    })
+})
